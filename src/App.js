bb9447d2ab39228e32c1e9608006d47261e5f745
Dani Paolo S. Dullano <pdullano@gmail.com>
2020-06-24 11:37:51 +0800

    updating packages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <h1>Hello from V2</h1>
+        <p>update the package</p>
       </header>
       <AmplifySignOut />
     </div>
